87b1ab8d48d7b7febfb488c6fa5d69fb0a1dec68
lacuba <nikolay.romanov164@gmail.com>
2025-11-16 23:59:00 +0300

    init

---
+++ site/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// const nodes: NodeData[] = [
+//     { id: "A", group: "g1" },
+//     { id: "B", group: "g1" },
+//     { id: "C", group: "g2" },
+//     { id: "D", group: "g2" },
+//     // ... thousands more
+// ];
+
+// const links: LinkData[] = [
+//     { source: "A", target: "B", label: "A→B #1" },
+//     { source: "A", target: "B", label: "A→B #2" },
+//     { source: "B", target: "C", label: "B→C" },
+//     { source: "C", target: "A", label: "C→A" },
+// ];
+
+export default function App() {
+  return <div style={{ width: '100%', height: '100%' }}></div>
+}
